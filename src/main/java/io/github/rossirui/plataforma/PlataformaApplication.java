047b9b1e343f85b879d14bbaf5dc699840adf582
Rui Rossi <rui.rossi@gmail.com>
2020-07-29 23:55:50 -0300

    Projeto Plataforma criado

---
+++ src/main/java/io/github/rossirui/plataforma/PlataformaApplication.java
@@ -0,0 +1,11 @@
+package io.github.rossirui.plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaApplication.class, args);
+    }
+}
